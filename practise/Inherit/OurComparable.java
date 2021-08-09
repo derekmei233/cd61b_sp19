c32016a02df6ec003e8e927099eaee72f6488096
derek mei <derekmei233@gmail.com>
2021-08-08 21:20:32 -0400

    init project1b

--- practise/Inherit/OurComparable.java
+++ practise/Inherit/OurComparable.java
@@ -1,9 +1,10 @@
-public interface OurComparable {
+public interface OurComparable<T> {
     /** Reture -1 if this < o
      *  Reture 0 if this equals o
      *  Reture 1 if this > o
      * @param o
      * @return
      */
-    public int compareTo(Object o);
+    public int compareTo(T o);
+    // or easier substract directly.
 }
