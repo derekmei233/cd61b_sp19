a895edb96a78b5e891971aa77d49ab0a04daefa7
derek mei <derekmei233@gmail.com>
2021-08-09 23:10:58 -0400

    final compile revisio

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -17,6 +17,17 @@ public class TestOffByOne {
         char tp3 = 'q';
         char tq3 = 's';
         assertFalse(offByOne .equalChars(tp3, tq3));
-
+        char tp4 = 'Q';
+        char tq4 = 'P';
+        assertTrue(offByOne .equalChars(tp4, tq4));
+        char tp5 = 'Q';
+        char tq5 = 'T';
+        assertFalse(offByOne .equalChars(tp5, tq5));
+        char tp6 = '*';
+        char tq6 = '(';
+        assertFalse(offByOne .equalChars(tp6, tq6));
+        char tp7 = '(';
+        char tq7 = ')';
+        assertTrue(offByOne .equalChars(tp7, tq7));
     }
 }
